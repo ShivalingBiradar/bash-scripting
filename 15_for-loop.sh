de881b601da4defd09a010909f56ed1111c52543
Shivaling Biradar <149060582+ShivalingBiradar@users.noreply.github.com>
2024-07-18 23:20:15 +0530

    Created 15_for-loop.sh

---
+++ 15_for-loop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+        echo "Number is $i"
+done
+
+for name in Raju Sham Baburao
+do
+        echo "Name is $name"
+done
+
+Output:
+Number is 1
+Number is 2
+Number is 3
+Number is 4
+Number is 5
+Name is Raju
+Name is Sham
+Name is Baburao
