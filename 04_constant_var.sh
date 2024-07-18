3f4c7dc0dde4cc0611be0778b92cabc2d107f1e7
Shivaling Biradar <149060582+ShivalingBiradar@users.noreply.github.com>
2024-07-18 22:54:24 +0530

    Created 04_constant_var.sh

---
+++ 04_constant_var.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Constant variable
+
+readonly COLLEGE="METRO"
+echo "My college name is $COLLEGE "
+
+COLLEGE="TEST"
+
+
+##Output:
+My college name is METRO
+04_constant_var.sh: line 8: COLLEGE: readonly variable
