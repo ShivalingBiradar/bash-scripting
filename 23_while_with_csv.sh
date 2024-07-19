ba3a99cb067f2b7d1d873305cfb7c627c8c5784f
Shivaling Biradar <149060582+ShivalingBiradar@users.noreply.github.com>
2024-07-19 18:24:05 +0530

    Created 23_while_with_csv.sh

---
+++ 23_while_with_csv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while IFS="," read id name age
+do
+        echo "Id is $id"
+        echo "name is $name"
+        echo "age is $age"
+done < test.csv
+
+
+Output:
+Id is id
+name is name
+age is age
+Id is 01
+name is shiva
+age is 20
+Id is 02
+name is Raju
+age is 30
+Id is 03
+name is sham
+age is 40
