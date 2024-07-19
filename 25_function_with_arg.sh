99a6d93066aa65c4ac4e7b1f832966995251750a
Shivaling Biradar <149060582+ShivalingBiradar@users.noreply.github.com>
2024-07-19 19:34:02 +0530

    Created 25_function_with_arg.sh

---
+++ 25_function_with_arg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+welcomeNote() {
+        echo "--------------"
+        echo "Welcome $1"
+        echo "Age is $2"
+        echo "-------------"
+
+}
+
+welcomeNote Raju 20
+welcomeNote Sham 30
+
+
+Output:
+--------------
+Welcome Raju
+Age is 20
+-------------
+--------------
+Welcome Sham
+Age is 30
+-------------
