22380eee3164470e430fe73a3c0064b18918ec5f
Shivaling Biradar <149060582+ShivalingBiradar@users.noreply.github.com>
2024-07-19 18:35:10 +0530

    Remove  ID age and name empty lines

--- 23_while_with_csv.sh
+++ 23_while_with_csv.sh
@@ -21,3 +21,24 @@ age is 30
 Id is 03
 name is sham
 age is 40
+
+
+cat test.csv | awk 'NR!=1 {print}' | while IFS="," read id name age
+
+do
+        echo "Id is $id"
+        echo "name is $name"
+        echo "age is $age"
+done
+
+
+Output:
+Id is 01
+name is shiva
+age is 20
+Id is 02
+name is Raju
+age is 30
+Id is 03
+name is sham
+age is 40
